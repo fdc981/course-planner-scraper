aa394830b8194758e22e35af64f11a83e1209d76
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-04-17 23:49:38 +0930

    refactored unit test, added test_page_with_group

--- extractor_test.py
+++ extractor_test.py
@@ -1,8 +1,22 @@
 import unittest
 from extract_from_html import Extractor
 
-f = open("/home/neb/Documents/projects/course-planner-scraper/data/snapshots/AGRIC - Agriculture & Natural Resource/2021-04-13/AGRIC 1520WT - Agricultural Production I - 105341+1+4120+1.html", 'r').read()
-e = Extractor(f)
-x = e.course_details_as_df()
-y = e.class_details_as_df()
-z = e.compile_df()
+class TestExtractorMethods(unittest.TestCase):
+    def test_page_with_class_details(self):
+        filename = "data/snapshots/AGRIC - Agriculture & Natural Resource/2021-04-13/AGRIC 1520WT - Agricultural Production I - 105341+1+4120+1.html"
+        with open(filename, 'r') as f:
+            e = Extractor(f.read())
+            x = e.course_details_as_df()
+            y = e.class_details_as_df()
+            z = e.compile_df()
+
+    def test_page_with_group(self):
+        filename = "data/snapshots/ANIML SC - Animal Science/2021-04-16/ANIML SC 1016RW - Principles in Animal Behaviour Welfare Ethics I - 104097+1+4120+1.html"
+        with open(filename, 'r') as f:
+            e = Extractor(f.read())
+            x = e.course_details_as_df()
+            y = e.class_details_as_df()
+            z = e.compile_df()
+
+if __name__ == "__main__":
+    unittest.main()
