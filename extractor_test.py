f3198b513aa430f93920b899469fb3783962dbb9
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-04-19 16:50:34 +0930

    added 2 new tests: single row and no class

--- extractor_test.py
+++ extractor_test.py
@@ -18,5 +18,21 @@ class TestExtractorMethods(unittest.TestCase):
             y = e.class_details_as_df()
             z = e.compile_df()
 
+    def test_page_with_single_row_of_class_details(self):
+        filename = "data/snapshots/ORALHLTH - Oral Health/2021-04-16/ORALHLTH 3201AHO - Dental & Health Science IIIOH Part 1 - 101509+1+4110+FY1.html"
+        with open(filename, 'r') as f:
+            e = Extractor(f.read())
+            x = e.course_details_as_df()
+            y = e.class_details_as_df()
+            z = e.compile_df()
+
+    def test_page_with_no_class_details(self):
+        filename = "data/snapshots/DEVT - Development Studies/2021-04-16/DEVT 2010EX - Coffey International Development Internship - 110040+1+4115+1.html"
+        with open(filename, 'r') as f:
+            e = Extractor(f.read())
+            x = e.course_details_as_df()
+            y = e.class_details_as_df()
+            z = e.compile_df()
+
 if __name__ == "__main__":
     unittest.main()
