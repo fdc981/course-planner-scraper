8e7ce49892549449a2b8b74dc4c5f590ae14737e
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-04-22 12:23:32 +0930

    added automatic retries for src.requests_wrapper.get

--- src/requests_wrapper.py
+++ src/requests_wrapper.py
@@ -1,13 +1,31 @@
 # Wrapper for requests.
 
 import requests
+import time
 
-def get(url):
+def get(url : str, retry_time : int = -1, double_time : bool = False) -> str:
+    """Wrapper for requests.get which requests again upon failure.
+
+    retry_time : integer value that determines the retry time in seconds. If negative, then
+                 this function will prompt the user whether to retry.
+
+    double_time : if true and retry_time is nonnegative, doubles retry_time after each retry"""
     while True:
         try:
             res = requests.get(url)
             return res
         except BaseException as e:
-            c = input("Requests ended with error: \"%s\". Try again [y/N]? " % str(e))
-            if c.lower() == 'y':
+            if retry_time < 0:
+                c = input("Requests ended with error: \"%s\". Try again [y/N]? " % str(e))
+                if c.lower() == 'y':
+                    print("Continuing...")
+                    continue
+                else:
+                    print("Stopping...")
+                    break
+            else:
+                print("Requests ended with error: \"%s\". Retrying in %d seconds." % (str(e), retry_time))
+                time.sleep(retry_time)
+                if double_time:
+                    retry_time *= 2
                 continue
