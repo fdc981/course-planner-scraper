047937741664509846a53ce4b8deac4e481355ed
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-04-28 17:39:59 +0930

    added csv compilation step to main driver file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.scraper import Scraper
+from src.prepare import compile_data
 
 s = Scraper()
 
@@ -7,3 +8,6 @@ s.get_course_list()
 
 print("Retrieving course pages...")
 s.get_courses_html()
+
+print("Compiling data...")
+compile_data()
