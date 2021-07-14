2237d6a7f380194b7253139d3ac26cfe4af422fd
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-14 11:56:12 +0930

    Implement tests for compiled data
    Previously the file for tests.compiled_data remained incomplete and outdated. This commit updates this file, copying the appropriate code from src.extractor.Extractor.__sanitise_data().

--- tests/compiled_data.py
+++ tests/compiled_data.py
@@ -2,74 +2,41 @@ import unittest
 import pandas as pd
 from pathlib import Path
 from src.extractor import Extractor
+import re
+import tests.expected_types as expected
 
 class TestCourseDataIntegrity(unittest.TestCase):
-    COURSE_COLS = ["Career",
-                   "Units",
-                   "Term",
-                   "Campus",
-                   "Contact",
-                   "Restriction",
-                   "Available for Study Abroad and Exchange",
-                   "Available for Non-Award Study",
-                   "Pre-Requisite",
-                   "Assessment",
-                   "Syllabus",
-                   "Course ID",
-                   "Co-Requisite",
-                   "Discovery Experience â Global",
-                   "Discovery Experience â Working",
-                   "Assumed Knowledge",
-                   "Incompatible",
-                   "Session",
-                   "Quota",
-                   "Discovery Experience â Community",
-                   "Biennial Course"]
-
     def test_existence_of_required_columns(self):
         course_details_path = Path("data/compiled_data/course_details/")
         for filename in course_details_path.iterdir():
-            df = pd.read_csv(str(filename))
-            for col in self.COURSE_COLS:
-                self.assertTrue(col in df.columns)
+            df = pd.read_csv(str(filename), dtype=str)
+            for col in df.columns:
+                assert col in expected.course_col_types, f"Unexpected column {col}."
+
+    def test_data_consistency(self):
+        course_details_path = Path("data/compiled_data/course_details/")
+        for filename in course_details_path.iterdir():
+            df = pd.read_csv(str(filename), dtype=str)
+            for col in df.columns:
+                for el in df[col]:
+                    assert re.match(expected.course_col_types[col], str(el)), f"Element `{el}` with type {type(el)} of column {col} did not pass regex test {expected.course_col_types[col]}."
 
 
 class TestClassDataIntegrity(unittest.TestCase):
-    CLASS_COLS = ["Class Nbr",
-                  "Section",
-                  "Size",
-                  "Available",
-                  "Dates",
-                  "Days",
-                  "Time",
-                  "Location",
-                  "Class Type",
-                  "Group",
-                  "Note",
-                  "Subject Area",
-                  "Catalogue Number",
-                  "Course Title",
-                  "Course ID"]
-
     def test_existence_of_required_columns(self):
         class_details_path = Path("data/compiled_data/class_details/")
         for filename in class_details_path.iterdir():
-            df = pd.read_csv(str(filename))
-            for col in self.CLASS_COLS:
-                self.assertTrue(col in df.columns, "Missing column '%s' in %s" % (col, str(filename)))
+            df = pd.read_csv(str(filename), dtype=str)
+            for col in df.columns:
+                assert col in expected.class_col_types, f"Unexpected column {col}."
 
-    def test_class_number_data(self):
+    def test_data_consistency(self):
         class_details_path = Path("data/compiled_data/class_details/")
         for filename in class_details_path.iterdir():
-            df = pd.read_csv(str(filename))
-
-            # column must not have missing values
-            na = df[df['Class Nbr'].isna()]
-            self.assertTrue(len(na) == 0, "%d missing values found in the Class Nbr column for %s" % (len(na), str(filename)))
-
-            # column cannot have non integer values
-            selected = df[~(df['Class Nbr'].str.isdigit())]
-            self.assertTrue(len(selected) == 0, "%d non-integer values found in the Class Nbr column for %s" % (len(selected), str(filename)))
+            df = pd.read_csv(str(filename), dtype=str)
+            for col in df.columns:
+                for el in df[col]:
+                    assert re.match(expected.class_col_types[col], str(el)), f"Element `{el}` with type {type(el)} of column {col} did not pass regex test {expected.class_col_types[col]}."
 
 
 if __name__ == "__main__":
