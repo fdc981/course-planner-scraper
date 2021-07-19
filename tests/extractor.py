17a7a81997b4dc9fdfd07be575653b320de54157
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-07-19 15:29:09 +0930

    Add test for page with missing 'Location' data
    The new test added to tests/extractor.py checks whether data can be successfully extracted for a page with missing data in the 'Location' column of the class details table.

--- tests/extractor.py
+++ tests/extractor.py
@@ -100,6 +100,14 @@ class TestExtractorMethods(unittest.TestCase):
             y = e.class_details_as_df()
             z = e.compile_df()
 
+    def test_page_with_missing_location_data(self):
+        filename = "data/snapshots/ORALHLTH - Oral Health/2021-07-19/ORALHLTH 2201BHO - Dental & Health Science IIOH Part 2 - 101502+1+4120+FY2.html"
+        with open(filename, 'r') as f:
+            e = Extractor(f.read())
+            x = e.course_details_as_df()
+            y = e.class_details_as_df()
+            z = e.compile_df()
+
 
 if __name__ == "__main__":
     unittest.main()
